a4b69d028e2be64f61b5d6540420e70e8907f442
gmzelias <gmzelias@gmail.com>
2019-06-24 13:03:39 -0400

    Port changed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
 // Import routes
 require('./routes')(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
