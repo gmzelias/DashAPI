e10c8a60ceae18b623639f3c800313d015668773
gmzelias <gmzelias@gmail.com>
2019-06-25 12:10:23 -0400

    WordPress config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
     }
   }],
 
-  deploy : {
+ /* deploy : {
     production : {
       user : 'ubuntu',
       host : 'ec2-18-237-86-164.us-west-2.compute.amazonaws.com',
@@ -25,6 +25,18 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/gmzelias/DashAPI.git',
       path : '/home/ubuntu/Code/DashAPI',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }*/
+
+  deploy : {
+    production : {
+      user : 'bitname',
+      host : 'ec2-18-237-86-164.us-west-2.compute.amazonaws.com',
+      key  : 'C:/Users/gmzel/Desktop/wordpress-DHV.pem',
+      ref  : 'origin/master',
+      repo : 'https://github.com/gmzelias/DashAPI.git',
+      path : '/home/bitnami/apps/DashAPI',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
