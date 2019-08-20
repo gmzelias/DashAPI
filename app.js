f93939ccd105cb0f6fb2b18db311dfbf3dd64566
gmzelias <gmzelias@gmail.com>
2019-08-20 12:04:40 -0400

    Port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 require('./routes')(app);
 
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
